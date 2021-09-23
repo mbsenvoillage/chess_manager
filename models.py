e1d6a436ed74a11813597c4b8aa78fc975e807bf
Yvon M <yvon.momboisse@knap.fr>
2021-09-23 18:35:18 +0200

    venv init and Player

---
+++ models.py
@@ -0,0 +1,30 @@
+from datetime import date, datetime
+from typing import List, Optional
+from pydantic import BaseModel, validator, PositiveInt
+from enum import Enum
+
+
+class Gender(Enum):
+    Male = 'M'
+    Female = 'F'
+
+
+
+class Player(BaseModel):
+    id: PositiveInt
+    first_name: str
+    last_name: str
+    birthdate: date
+    ranking: PositiveInt
+    gender: Gender
+
+    # @validator('id')
+    # def id_must_be_positive(cls, value):
+    #     if value < 0:
+    #         raise ValueError('id must be positive integers')
+    #     return value
+
+    
+ 
+
+print(Player(id=2, first_name='John', last_name='Doe', birthdate='1989-03-30', ranking=2789, gender=Gender.Male))
