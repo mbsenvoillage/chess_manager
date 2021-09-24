2edf0be92e042fc9ecd2772b82ed26ef683baf3b
Yvon Momboisse <momboisse.yvn@gmail.com>
2021-09-24 16:19:50 +0200

    Added id count management

--- player.py
+++ player.py
@@ -1,19 +1,15 @@
 from datetime import date, datetime
-from typing import List, Optional
+from typing import ClassVar, List, Optional
 from pydantic import BaseModel, validator, PositiveInt
+from pydantic.fields import PrivateAttr
 from pydantic.types import constr
 from enums import Gender
 from dateutil.relativedelta import relativedelta
-import re
-
-
-
-
-
-
+import itertools
 
 class Player(BaseModel):
-    id: PositiveInt
+    _id_count = itertools.count(1)
+    id: PositiveInt = PrivateAttr
     # [TODO] first name must not contain more than 25 letters and less than 2
     # may contain spaces, accents, dashes
     # may not contain numbers, special charactersc
@@ -24,6 +20,7 @@ class Player(BaseModel):
     ranking: PositiveInt
     gender: Gender
 
+
     @validator('birthdate')
     def player_must_be_at_least_six(cls, value):
         if value > (date.today() - relativedelta(years=6)):
@@ -31,8 +28,12 @@ class Player(BaseModel):
         return value
 
 
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+        self.id = next(Player._id_count)
+
     
 
 
-print(Player(id=2, first_name='John90àç', last_name='Doe', birthdate='2018-03-30', ranking=2789, gender=Gender.Male))
-
+print(Player(first_name='John90àç', last_name='Doe', birthdate='2010-03-30', ranking=2789, gender=Gender.Male))
+print(Player(first_name='JORDI', last_name='SHORE', birthdate='2010-03-30', ranking=2789, gender=Gender.Male))
