e8b62e643ff72a48bfdc8bdca2f44a8629a66d9a
Yvon Momboisse <momboisse.yvn@gmail.com>
2021-09-24 15:05:48 +0200

    Added birthdate validator

--- player.py
+++ player.py
@@ -1,7 +1,11 @@
 from datetime import date, datetime
 from typing import List, Optional
 from pydantic import BaseModel, validator, PositiveInt
+from pydantic.types import constr
 from enums import Gender
+from dateutil.relativedelta import relativedelta
+import re
+
 
 
 
@@ -10,19 +14,25 @@ from enums import Gender
 
 class Player(BaseModel):
     id: PositiveInt
-    first_name: str
-    last_name: str
+    # [TODO] first name must not contain more than 25 letters and less than 2
+    # may contain spaces, accents, dashes
+    # may not contain numbers, special charactersc
+    first_name: constr(strip_whitespace=True, min_length=2, max_length=25)
+    last_name: constr(strip_whitespace=True, min_length=2, max_length=25)
+    #players must be at least 6
     birthdate: date
     ranking: PositiveInt
     gender: Gender
 
-    # @validator('id')
-    # def id_must_be_positive(cls, value):
-    #     if value < 0:
-    #         raise ValueError('id must be positive integers')
-    #     return value
+    @validator('birthdate')
+    def player_must_be_at_least_six(cls, value):
+        if value > (date.today() - relativedelta(years=6)):
+            raise ValueError('Player must be at least six years old')
+        return value
+
 
     
- 
 
-print(Player(id=2, first_name='John', last_name='Doe', birthdate='1989-03-30', ranking=2789, gender=Gender.Male))
+
+print(Player(id=2, first_name='John90àç', last_name='Doe', birthdate='2018-03-30', ranking=2789, gender=Gender.Male))
+
