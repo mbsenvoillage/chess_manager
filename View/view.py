23085201e63b423ec4389babb71b1bcf7a410025
Yvon M <yvon.momboisse@knap.fr>
2021-10-12 13:37:10 +0200

    - Reorganising view structure - Added static data for form in store

--- View/view.py
+++ View/view.py
@@ -23,19 +23,16 @@ class ViewOption(ABC):
 class View(ABC):
     """View parent class"""
     title: str 
-    info: List[str] 
-    selectable_options: List[ViewOption]
+    info: List[str]  
     _view_manager: ViewManager
     _page_layout: str = field(default=os.getenv('PAGE_LAYOUT'))
  
-    def __init__(self, view_manager, content_loader, key_of_view_to_be_loaded_from_store: str) -> None:
+    def __init__(self, view_manager) -> None:
         super().__init__()
         # attributes have to be initialized here since get_own_attributes cannot list uninitialized attributes (dir() does not offer the possibility)
         self.title = ''
         self.info = []
-        self.selectable_options = []
         self._view_manager = view_manager
-        content_loader.load_content(self, key_of_view_to_be_loaded_from_store)
 
     def get_own_attributes(self):
         """Lists attributes of the view instance"""
@@ -44,7 +41,7 @@ class View(ABC):
     @abstractmethod
     def render(self):
         """Renders the view"""
-        pass
+        os.system('cls' if os.name == 'nt' else 'reset')
 
     @abstractmethod
     def submit():
@@ -54,19 +51,37 @@ class View(ABC):
     def format_info(self, format_helper_func):
         return format_helper_func(self.info)
         
-    def format_selectable_options(self, format_helper_func):
-        return format_helper_func(self.selectable_options)
     
 class Menu(View):
-    
-    def submit(self, userInput):
-            self._view_manager.parseInput(userInput)
+
+    selectable_options: List[ViewOption]
+
+    def __init__(self, view_manager, content_loader, key_of_view_to_be_loaded_from_store: str) -> None:
+        super().__init__(view_manager)
+        self.selectable_options = []
+        content_loader.load_content(self, key_of_view_to_be_loaded_from_store)
 
     def render(self):
-        os.system('cls' if os.name == 'nt' else 'reset')
+        super().render()
         formatted_info = self.format_info(view_info_formatter)
         formatted_options = self.format_selectable_options(view_selectable_options_formatter)
         print(self._page_layout.format(self.title, formatted_info, formatted_options))    
         userInput = input()
         self.submit(userInput)
-  
+
+    
+    def submit(self, userInput):
+            self._view_manager.parseInput(userInput)
+
+    def format_selectable_options(self, format_helper_func):
+        return format_helper_func(self.selectable_options)
+
+    
+
+class Form(View):
+    form_fields: List[ViewOption]
+
+
+    def render(self):
+        pass
+        
--- store.py
+++ store.py
@@ -18,19 +18,21 @@ static_view_content = {
         'title': 'Player Menu',
         'info': ['Create a new player, or edit an existing one.', 'Pick an option from the list below and press enter.', 'Press q and enter to navigate to main menu'],
         'selectable_options': [{'text': '1. Create Player', 'route': '/player/create'}, {'text' : '2. Edit Player', 'route': '/player/edit'}],
-      
     },
     'PLAYER_EDIT': {
         'title': 'Edit Players',
         'info': ['Select a player to edit any data from its profile, like ranking.', 'Pick an option from the list below and press enter.', 'Press q and enter to navigate to main menu'],
         'selectable_options':[],
-        
     },
     'TOURNAMENT_MENU': {
         'title': 'Tournament Menu',
         'info': ['Create a new tournament, or edit an existing one.','Pick an option from the list below and press enter.', 'Press q and enter to navigate to main menu'],
         'selectable_options': [{'text': '1. Create Tournament', 'route': '/tournament/create'}, {'text' : '2. Edit Tournament', 'route': '/tournament/edit'}],
-       
+    },
+    'PLAYER_CREATE': {
+        'title': 'Add New Player',
+        'info': 'All form fields must be filled in order to add the new player to the database',
+        'form_fields': ["Player's first name : ", "Player's last name : ", "Player's date of birth (yyyy-mm-dd) : ", "Player's gender (M/F) : ", "Player's ranking : "]
     }
 }
 
